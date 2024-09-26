dc51f81c3a0659159e1ad4b4ebdcd0d16c1b6a05
Valeria Fedorchuk <74813522+Fedorchuk-Valeria@users.noreply.github.com>
2024-09-26 18:08:54 +0300

    Add files via upload
    calendar v2

--- calendar.js
+++ calendar.js
@@ -9,25 +9,25 @@ window.addEventListener("load", (e) => {
     const weekDay = getCurrWeekDay()[1];
 
     document.getElementById("headerDay").innerText = weekDay + ' ' + getCurrDayDate();
+    
 
     const currDay = Number(getCurrDayDate().slice(0, 2))
     fillCalendarCard(currYear, getCurrMonth()[0], getCurrMonth()[1], currDay, document)
+    fillEventInfo(getCurrMonth()[0], currDay)
 })
 
 
 document.getElementById('calendarDays').addEventListener('click', function(e) {
     if (e.target.tagName === 'LI'){
         const day = e.target.innerText;
-        document.getElementById("eventInfo").innerHTML = ""
-        getEventByDate(sessionStorage.getItem("currMonth"), day).then((events) => {
-            for (let i = 0; i < events.length; i++) {
-                let res = "<p>" + events[i].name + "</p>"
-                res += "<p>" + events[i].description + "</p>"
-                document.getElementById("eventInfo").innerHTML += res;
-            }
-        })
+        fillEventInfo(sessionStorage.getItem("currMonth"), day)
     }
 });
+
+
+document.getElementById("profileButton").addEventListener("click", (e) => {
+    window.location.href = './profile.html';
+  })
 
 
 function fillCalendarCard(year, month, monthName, currDay, doc){
@@ -58,4 +58,28 @@ function fillCalendarCard(year, month, monthName, currDay, doc){
             }
         }
     })
+}
+
+function fillEventInfo(month, day){
+    let date = day + '.' + (Number(month) + 1)
+    if(date.length == 4){
+        date = date.slice(0, 3) + '0' + date.slice(3)
+    }
+    document.getElementById("eventInfo").innerHTML = 
+    "<h3 style='color: #7D8994; text-align: center'>Запланированные мероприятия <br> на "
+    + date + "</h3>"
+        getEventByDate(month, day).then((events) => {
+            for (let i = 0; i < events.length; i++) {
+                let eventDate = new Date(events[i].date * 1000);
+                let hours = eventDate.getHours().toString();
+                let minutes = eventDate.getMinutes().toString();
+                if(minutes.length == 1){
+                    minutes = '0' + minutes.toString()
+                }
+                let res = "<b>" + hours + ':' + minutes + "</b>"
+                res += "<span>" + events[i].name + "</span>"
+                res += "<i>" + events[i].description + "</i>"
+                document.getElementById("eventInfo").innerHTML += res;
+            }
+        })
 }
